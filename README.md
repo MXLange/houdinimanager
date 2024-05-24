fc2d6b238d4ef39050139f584a6983020a844c6c
MXLange <lange_sp@hotmail.com>
2024-05-23 23:08:07 -0300

    houdini manager

---
+++ README.md
@@ -0,0 +1,48 @@
+This is a go lib to manage your go routines.
+
+Here is an example to test:
+
+```go
+package main
+
+import (
+	"fmt"
+	"houdinimanager"
+	"log"
+	"time"
+)
+
+func main() {
+
+	houdini, err := houdinimanager.NewHoudiniManager(true, true, 2)
+	if err != nil {
+		log.Fatalf("Erro ao criar o gerenciador de rotinas: %v", err.Error())
+	}
+
+	for i := 1; i <= 5; i++ {
+		houdini.Execute(func() {
+			teste(i, "true, true, 5")
+		})
+	}
+	houdini.Wait()
+
+	houdini2, err := houdinimanager.NewHoudiniManager(false, false, 0)
+	if err != nil {
+		log.Fatalf("Erro ao criar o gerenciador de rotinas: %v", err.Error())
+	}
+
+	for i := 1; i <= 50; i++ {
+		houdini2.Execute(func() {
+			teste(i, "false, false, 0")
+		})
+	}
+	houdini2.Wait() //this will not make the program wait for the houdinis to finish
+
+	fmt.Println("The second case didn't complete because the program didn't wait for the houdinis to finish")
+}
+
+func teste(i int, config string) {
+	fmt.Printf("Executando rotina %v, config %v\n", i, config)
+	time.Sleep(time.Duration(i) * time.Second)
+	fmt.Printf("Rotina %v finalizada, config %v\n", i, config)
+}
---
+++ houdini_manager.go
@@ -0,0 +1,88 @@
+package houdinimanager
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+type HoudiniManager struct {
+	routineCounter int
+	maxRoutines    int
+	syncGroup      sync.WaitGroup
+	needToWait     bool
+}
+
+func NewHoudiniManager(setMaxRoutines, needToWait bool, maxRoutines int) (*HoudiniManager, error) {
+
+	if setMaxRoutines && maxRoutines <= 0 {
+		return nil, errors.New("if you need to set it, max houdinis must be greater than 0")
+	} else if !setMaxRoutines && maxRoutines > 0 {
+		return nil, errors.New("if you don't need to set it, max houdinis must be 0")
+	}
+
+	manager := &HoudiniManager{
+		routineCounter: 0,
+		needToWait:     needToWait,
+	}
+
+	if setMaxRoutines {
+		manager.maxRoutines = maxRoutines
+	}
+
+	manager.syncGroup = sync.WaitGroup{}
+
+	return manager, nil
+}
+
+func (r *HoudiniManager) Execute(f func()) {
+	r.waitAvailableHoudini()
+	r.addCount()
+	r.addWait()
+	go func(r *HoudiniManager) {
+		defer r.reduceCount()
+		defer r.doneWait()
+		f()
+	}(r)
+}
+
+func (r *HoudiniManager) addWait() {
+	if r.needToWait {
+		r.syncGroup.Add(1)
+	}
+}
+
+func (r *HoudiniManager) doneWait() {
+	if r.needToWait {
+		r.syncGroup.Done()
+	}
+}
+
+func (r *HoudiniManager) Wait() {
+	if r.needToWait {
+		r.syncGroup.Wait()
+	}
+}
+
+func (r *HoudiniManager) PrintHoudiniCounter() {
+	println("Houdini counter: ", r.routineCounter)
+}
+
+func (r *HoudiniManager) waitAvailableHoudini() {
+	if r.maxRoutines > 0 {
+		for r.routineCounter == r.maxRoutines {
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+func (r *HoudiniManager) addCount() {
+	if r.maxRoutines > 0 {
+		r.routineCounter++
+	}
+}
+
+func (r *HoudiniManager) reduceCount() {
+	if r.maxRoutines > 0 {
+		r.routineCounter--
+	}
+}
